c01dc28a56afb6824afb7c04d05bb216adf7acac
bnuzhengshan <396653035@qq.com>
2020-02-08 19:32:05 +0800

    Initial the page of project

---
+++ build/precache-manifest.6c8f8b9798780a4fb7d54d59ba42592c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8740f5d75fcce7ffd091ef0754b042a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7332ce5d8587821c4b85",
+    "url": "/static/css/main.a489efbd.chunk.css"
+  },
+  {
+    "revision": "ded17e83703a83dce51c",
+    "url": "/static/js/2.642d0cb4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.642d0cb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7332ce5d8587821c4b85",
+    "url": "/static/js/main.948dec7d.chunk.js"
+  },
+  {
+    "revision": "d705181d7938f2b9793c",
+    "url": "/static/js/runtime-main.aba12976.js"
+  }
+]);
