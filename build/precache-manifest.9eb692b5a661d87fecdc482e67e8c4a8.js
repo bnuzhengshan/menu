907e51c9b59a48bfe56b6a8e2700adff388b8d7b
bnuzhengshan <396653035@qq.com>
2020-02-08 20:40:21 +0800

    re commit build

---
+++ build/precache-manifest.9eb692b5a661d87fecdc482e67e8c4a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2c84757bbfb9ab8a5f44bdc54583d22",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cbbdd8d94dfe346e2f29",
+    "url": "./static/css/main.744a64fa.chunk.css"
+  },
+  {
+    "revision": "ded17e83703a83dce51c",
+    "url": "./static/js/2.642d0cb4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.642d0cb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbbdd8d94dfe346e2f29",
+    "url": "./static/js/main.3f20c624.chunk.js"
+  },
+  {
+    "revision": "529ae6d0dce363905041",
+    "url": "./static/js/runtime-main.0ded9d46.js"
+  }
+]);
