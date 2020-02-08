a7a81b7ed35135324e130c3cf2aa53a4b4f1255c
bnuzhengshan <396653035@qq.com>
2020-02-08 20:23:15 +0800

    调整样式

--- src/component/modal.js
+++ src/component/modal.js
@@ -7,27 +7,32 @@ class Modal extends React.Component {
     render() {
         const { title } = this.props;
         return (
-            <div className={this.props.show ? 'modal ' : 'modal hidden'}>
+            <div className={this.props.show ? 'modal-root ' : 'modal-root hidden'}>
                 <div className='modal-mask'></div>
-                <div className='modal-content'>
-                    <div className='modal-header'>
-                        <span className='modal-title'>{title}</span>
-                        <div className='modal-close'>
-                            <i onClick={this.props.onClose} className="iconfont iconguanbi"></i>
+                <div className='modal-wrap'>
+                    <div className='modal'>
+                        <div className='modal-content'>
+                            <div className='modal-header'>
+                                <span className='modal-title'>{title}</span>
+                                <div className='modal-close'>
+                                    <i onClick={this.props.onClose} className="iconfont iconguanbi"></i>
+                                </div>
+                            </div>
+                            <div className='modal-body'>
+                                {this.props.children}
+                            </div>
+                            <div className='modal-footer'>
+                                <button onClick={this.props.onClose} className='button'>
+                                    <span>取消</span>
+                                </button>
+                                <button onClick={this.props.onOk} className='button button-primary'>
+                                    <span>确定</span>
+                                </button>
+                            </div>
                         </div>
                     </div>
-                    <div className='modal-body'>
-                        {this.props.children}
-                    </div>
-                    <div className='modal-footer'>
-                        <button onClick={this.props.onClose} className='button'>
-                            <span>取消</span>
-                        </button>
-                        <button onClick={this.props.onOk} className='button button-primary'>
-                            <span>确定</span>
-                        </button>
-                    </div>
                 </div>
+
             </div>
         )
     }
